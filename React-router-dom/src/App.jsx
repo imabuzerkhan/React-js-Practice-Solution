7c95c215697669c9429630e4f5d70980f3e01b5e
imabuzerkhan <abuzerk41@gmail.com>
2024-05-20 10:02:59 +0545

    new commit

--- React-router-dom/src/App.jsx
+++ React-router-dom/src/App.jsx
@@ -1,29 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './Home';
-import About from './About';
+import Header from './Header';
+import Footer from './Footer';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
-    </Router>
+   <>
+<Header/>
+<Outlet></Outlet>
+<Footer></Footer>
+   </>
   );
 }
 
--- React-router-dom/src/main.jsx
+++ React-router-dom/src/main.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import {
+  Route,
+  RouterProvider,
+  Routes,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Home from "./Home.jsx";
+import About from "./About.jsx";
+import Contact from "./Contact.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path : "/",
+    element : <App/>,
+    children : [
+      {
+path : "",
+element : <Home/>
+      },
+      {
+        path : "About",
+        element: <About/>
+      },
+      {
+        path : "Contact",
+        element : <Contact/>
+      }
+    ]
+  }
+])
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Routes>
+// <Route path="/ " Element={<App />}/>
+//       <Route path=" " Element={<Home />} />
+//       <Route path="About " Element={<About />} />
+//       <Route path="Contact " Element={<Contact />} />
+//     </Routes>
+    
+    
+//   )
+// );
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
